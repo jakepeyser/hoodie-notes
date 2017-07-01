6be405f26a102b06f9245d6515e0e0cd7ec4f12a
Jake Peyser <jakepeyser@gmail.com>
2017-07-01 14:24:35 -0400

    gave hoodie client an extensible url

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -5,7 +5,7 @@ import status from './connection'
 // Create Hoodie client instance
 import Hoodie from '@hoodie/client'
 const hoodieInstance = new Hoodie({
-  url: 'http://localhost:8080',
+  url: window.location.origin,
   PouchDB: require('pouchdb-browser')
 })
 const hoodie = () => hoodieInstance
