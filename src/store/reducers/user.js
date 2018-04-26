50a3d18ed32bbd555a44debf2114936fe4a0d447
Jake Peyser <jakepeyser@gmail.com>
2018-04-26 12:13:54 -0400

    sign in after sign up - fixes #10

--- src/store/reducers/user.js
+++ src/store/reducers/user.js
@@ -1,65 +1,70 @@
 /* -----------------    ACTIONS     ------------------ */
 
-const LOGIN = 'LOGIN'
-export const LOGOUT = 'LOGOUT'
+const LOGIN = 'LOGIN';
+export const LOGOUT = 'LOGOUT';
 
 /* ------------   ACTION CREATORS     ------------------ */
 
-const authenticated = account => ({ type: LOGIN, account })
-const loggedOut = account => ({ type: LOGOUT })
+const authenticated = account => ({ type: LOGIN, account });
+const loggedOut = account => ({ type: LOGOUT });
 
 /* ------------       DISPATCHERS     ------------------ */
 
 // Check session. If logged in, "authenticate"
 export const checkUser = () => (dispatch, getState) => {
-  getState().hoodie.account.get()
+  getState()
+    .hoodie.account.get()
     .then(({ session, id, username }) => {
-      if (session) dispatch(authenticated({ id, username }))
+      if (session) dispatch(authenticated({ id, username }));
     })
-    .catch(err => console.error(err))
-}
-
-// Create a new user
-export const createUser = (username, password, cb) => (dispatch, getState) => {
-  getState().hoodie.account.signUp({ username, password })
-    .then(({ id, username }) => {
-      dispatch(authenticated({ id, username }))
-      cb()
-    }).catch(err => {
-      console.error(err)
-      cb(err)
-    })
-}
+    .catch(err => console.error(err));
+};
 
 // Log in to an existing account
 export const login = (username, password, cb) => (dispatch, getState) => {
-  getState().hoodie.account.signIn({ username, password })
+  getState()
+    .hoodie.account.signIn({ username, password })
     .then(account => {
-      dispatch(authenticated(account))
-      cb()
-    }).catch(err => {
-      console.error(err)
-      cb(err)
+      dispatch(authenticated(account));
+      cb();
     })
-}
+    .catch(err => {
+      console.error(err);
+      cb(err);
+    });
+};
+
+// Create a new user
+export const createUser = (username, password, cb) => (dispatch, getState) => {
+  getState()
+    .hoodie.account.signUp({ username, password })
+    .then(({ id, username }) => {
+      dispatch(login(username, password, cb));
+    })
+    .catch(err => {
+      console.error(err);
+      cb(err);
+    });
+};
 
 // Log out of the current session
 export const logout = () => (dispatch, getState) => {
-  getState().hoodie.account.signOut()
+  getState()
+    .hoodie.account.signOut()
     .then(() => dispatch(loggedOut()))
-    .catch(err => console.error(err))
-}
+    .catch(err => console.error(err));
+};
 
 /* --------------      REDUCER     ------------------ */
 
-const initialAccount = null
+const initialAccount = null;
 export default function reducer(account = initialAccount, action) {
   switch (action.type) {
     case LOGIN:
-      return action.account
+      return action.account;
     case LOGOUT:
-      return null
+      return null;
     default:
-      return account
+      return account;
   }
 }
